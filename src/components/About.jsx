6008d2174f574fa102b8a205243234215a599ce4
kaziabulhasib <hasibkazi420@gmail.com>
2024-08-25 21:37:16 +0530

    about update 2

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
 const About = () => {
   return (
     <div className='my-24'>
-      <h1 className='text-3xl text-center'>About page</h1>
+      <h1 className='text-3xl text-center text-gray-700'>About page</h1>
     </div>
   );
 };
